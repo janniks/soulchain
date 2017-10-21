474c9b689fd7715f67a5a324923429c6a70e6276
janniks <jannik.siebert@gmail.com>
2017-10-21 15:38:04 +0200

    Add facebook routing

---
+++ app/controllers/social.js
@@ -0,0 +1,50 @@
+var router = ibk.router();
+
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+
+//# API ########################################################################
+
+//## Social ####################################################################
+
+//### Routes ###################################################################
+router.use(passport.initialize());
+router.use(passport.session());
+
+//### Handlers #################################################################
+passport.use(new Strategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: 'http://localhost:8080/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    log.info('profile', profile);
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  log.info('user', user);
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  log.info('obj', obj);
+  cb(null, obj);
+});
+
+router.get('/login', function(req, res){
+	res.render('login');
+});
+
+router.get('/login/facebook', passport.authenticate('facebook'));
+
+router.get('/login/facebook/return', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
+	res.redirect('/');
+});
+
+router.get('/profile', require('connect-ensure-login').ensureLoggedIn(), function(req, res){
+	res.render('profile', { user: req.user });
+});
+
+// Export router
+module.exports = router;
